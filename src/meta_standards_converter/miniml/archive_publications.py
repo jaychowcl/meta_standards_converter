9c55c2eecffbcd1229395bb127d51cd744b4dae8
jaychowcl <jaychowcl@gmail.com>
2026-09-13 19:57:37 +0100

    Add required author headers to archive citation modules and tests

--- src/meta_standards_converter/miniml/archive_publications.py
+++ src/meta_standards_converter/miniml/archive_publications.py
@@ -1,3 +1,11 @@
+# =============================================================================
+# Authors
+#
+# Created by jaychowcl @ Saez-Rodriguez Group & EMBL-EBI Functional Genomics Team on May 2026
+# https://github.com/jaychowcl
+# https://saezlab.org
+# https://www.ebi.ac.uk/about/teams/functional-genomics/
+# =============================================================================
 """Pure citation projection, retaining the scope of each explicit source link."""
 from copy import deepcopy
 from ..sources.archive_publications import references, study_accessions
